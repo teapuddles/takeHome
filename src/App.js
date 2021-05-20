51ca13fb576ac7c1a2f1f4b91b84dccd421358fd
teapuddles <kingsley.gleeson@gmail.com>
2021-05-19 20:09:53 -0400

    bringing over worked on code to repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState, useMemo} from 'react';
+import Summary from './components/Summary.js'
+
+import { shuffle } from './helpers.js'
+import { quizzes } from './data/quizzes.js'
 
 const App = () => {
-  return <h1>HELLO</h1>;
+  return (
+    <div className='app'>
+    {/* {showSummary ? (
+      // ternary for showing the Summary page. 
+    )}  */}
+  <h1>HELLO</h1>
+  </div>
+  )
 };
+
 
 export default App;
---
+++ src/components/Summary.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Summary = () => {
+    return(
+        <h1>"THE END"</h1>
+    )
+}
+
+export default Summary
