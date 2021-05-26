155e22b0be5def418c64ea4acc0a5f71809f4254
teapuddles <kingsley.gleeson@gmail.com>
2021-05-26 16:28:04 -0400

    reformatting and adding css

--- src/App.js
+++ src/App.js
@@ -1,162 +1,171 @@
-import React, {useState, useMemo} from 'react';
-import Summary from './components/Summary.js'
+import React, { useState, useMemo } from 'react';
+import Summary from './components/Summary.js';
 
-import { shuffle } from './helpers.js'
-import { quizzes } from './data/quizzes.js'
-import { getMessage } from './data/messages.js'
+import { shuffle } from './helpers.js';
+import { quizzes } from './data/quizzes.js';
+import { getMessage } from './data/messages.js';
 
 const App = () => {
-
   // state variables
-  const [currentQuiz, setCurrentQuiz] = useState(0)
-  const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [score, setScore] = useState(0)
-  const [attempts, setAttempts] = useState(0)
+  const [currentQuiz, setCurrentQuiz] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswer, setUserAnswer] = useState('');
+  const [score, setScore] = useState(0);
+  const [attempts, setAttempts] = useState(0);
 
-  const [showSummary, setShowSummary] = useState(false)
-  const [nextButton, setNextButton] = useState(false)
-  const [answerFeedback, setAnswerFeedback] = useState(null)
-
+  const [showSummary, setShowSummary] = useState(false);
+  const [nextButton, setNextButton] = useState(false);
+  const [answerFeedback, setAnswerFeedback] = useState(null);
 
   // combines and shuffles answers with Fischer/Yates helper
-  const answers = useMemo (() => {
-    const incorrectOptions = quizzes[currentQuiz].questions[currentQuestion].incorrectAnswers
-    const correctOptions = quizzes[currentQuiz].questions[currentQuestion].correctAnswer
+  const answers = useMemo(() => {
+    const incorrectOptions =
+      quizzes[currentQuiz].questions[currentQuestion].incorrectAnswers;
+    const correctOptions =
+      quizzes[currentQuiz].questions[currentQuestion].correctAnswer;
 
-    const combinedOptions = [...incorrectOptions, correctOptions]
+    const combinedOptions = [...incorrectOptions, correctOptions];
 
-    return shuffle(combinedOptions)
+    return shuffle(combinedOptions);
+  }, [currentQuiz, currentQuestion]);
 
-  }, [currentQuiz, currentQuestion])
-
-
-  // handleAnswerClick sends to handleAnswer 
-  const handleAnswerClick = (e, answer) => {
-    if(!answerFeedback) {
-      handleAnswer(e, answer)
-    } 
-  }
-
-    // getClasses adds the classes of correct/incorrect
-    const getClasses = (currentAnswer) => {
-      if (nextButton) {
-        if (
-          currentAnswer ===
-          quizzes[currentQuiz].questions[currentQuestion].correctAnswer
-        ) {
-          return ' correct-answer';
-        } else if (userAnswer === currentAnswer) {
-          return ' incorrect-answer';
-        }
+  // getClasses adds the classes of correct/incorrect
+  const getClasses = (currentAnswer) => {
+    if (nextButton) {
+      if (
+        currentAnswer ===
+        quizzes[currentQuiz].questions[currentQuestion].correctAnswer
+      ) {
+        return ' correct-answer';
+      } else if (userAnswer === currentAnswer) {
+        return ' incorrect-answer';
       }
-      return '';
-    };
+    }
+    return '';
+  };
 
+  // handleAnswerClick sends to handleAnswer
+  const handleAnswerClick = (e, answer) => {
+    if (!answerFeedback) {
+      handleAnswer(e, answer);
+    }
+  };
 
   const handleAnswer = (e, answer) => {
-    e.target.style.pointerEvents = 'none'
-    e.target.style.borderColor = 'green'
+    e.target.style.pointerEvents = 'none';
+    setUserAnswer(answer);
+    setNextButton(true);
+    const theCorrectAnswer =
+      quizzes[currentQuiz].questions[currentQuestion].correctAnswer;
 
-    const theCorrectAnswer = quizzes[currentQuiz].questions[currentQuestion].correctAnswer
-
-    setNextButton(true)
-  
-    if( answer === theCorrectAnswer ){
-      const newScore = score + 1
-      setScore(newScore)
-      setAnswerFeedback('Correct!')
-
+    if (answer === theCorrectAnswer) {
+      const newScore = score + 1;
+      setScore(newScore);
+      setAnswerFeedback('Correct!');
     } else {
-      e.target.style.borderColor = 'red'
-      e.target.style.textDecoration = 'line-through'
-      e.target.style.textDecorationColor = 'black'
-      
-      setAnswerFeedback('Incorrect...')
-
+      setAnswerFeedback('Incorrect...');
     }
-  }
-
+  };
 
   // Move to Next Question Click
   const handleNextQuestionClick = () => {
+    const nextQuestion = currentQuestion + 1;
+    setUserAnswer('');
+    setAnswerFeedback(null);
+    setCurrentQuestion(nextQuestion);
 
-    const nextQuestion = currentQuestion + 1
-      setAnswerFeedback(null)
-      setCurrentQuestion(nextQuestion)
-
-    if(nextQuestion < quizzes[currentQuiz].questions.length){
-      setCurrentQuestion(nextQuestion)
-      setNextButton(false)
-	
-		}else{
-      setCurrentQuestion(0)
-      setShowSummary(true)
+    if (nextQuestion < quizzes[currentQuiz].questions.length) {
+      setCurrentQuestion(nextQuestion);
+      setNextButton(false);
+    } else {
+      setCurrentQuestion(0);
+      setShowSummary(true);
       // at the end of quiz add to attempts
-      const newAttempt = attempts + 1
-      setAttempts(newAttempt)
-		}
-  }
-
+      const newAttempt = attempts + 1;
+      setAttempts(newAttempt);
+    }
+  };
 
   // Move to Next Quiz click, passed to Summary
   const handleNextQuizClick = () => {
+    const nextQuiz = currentQuiz + 1;
+    setCurrentQuiz(nextQuiz);
 
-    const nextQuiz = currentQuiz + 1
-    setCurrentQuiz(nextQuiz)
-
-    if(nextQuiz < quizzes.length){
-      setCurrentQuiz(nextQuiz)
-      setScore(0)
-      setAttempts(0)
-      setNextButton(false)
-      setShowSummary(false)
-
-    }else{
-      setCurrentQuiz(0)
-      setScore(0)
-      setAttempts(0)
-      setNextButton(false)
-      setShowSummary(false)
+    if (nextQuiz < quizzes.length) {
+      setCurrentQuiz(nextQuiz);
+      setScore(0);
+      setAttempts(0);
+      setNextButton(false);
+      setShowSummary(false);
+    } else {
+      setCurrentQuiz(0);
+      setScore(0);
+      setAttempts(0);
+      setNextButton(false);
+      setShowSummary(false);
     }
-  }
+  };
 
-
- // Retake Click, passed to Summary 
+  // Retake Click, passed to Summary
   const handleRetakeClick = () => {
-    setCurrentQuestion(0)
-    setScore(0)
-    setNextButton(false)
-    setShowSummary(false)
-  }
-  
+    setCurrentQuestion(0);
+    setScore(0);
+    setNextButton(false);
+    setShowSummary(false);
+  };
 
   return (
-    <div className='app'>
-       <div className='quiz-title'>{quizzes[currentQuiz].title}</div>
-
-        {showSummary ? (
-      <Summary score={score} attempts={attempts} quizTotal={quizzes[currentQuiz].questions} handleNextQuizClick={handleNextQuizClick} handleRetakeClick={handleRetakeClick} getMessage={getMessage}/>
-    ) : (
-
-      <>
-      <br></br>
-        <div className='question-section'>
-        <div className='question-text'>{quizzes[currentQuiz].questions[currentQuestion].text}</div>
-      <br></br>
-        <div className='answer-section' key={currentQuestion} >
-        {answers.map((answer) => 
-            <li className={`answer-list${getClasses(answer)}`}
-            key={`${currentQuestion}-${answer}`} onClick={(e) => handleAnswerClick(e, answer)}>{answer}</li>
-        )}
-      </div>
-      </div>
-        <h3 className='correct-incorrect-message' style={{display : answerFeedback ? 'block' : 'none'}}>{answerFeedback}</h3>
-        <h3 className='next-question' style={{display: nextButton ? 'block' : 'none'}} onClick={() => handleNextQuestionClick()}> Next </h3>
-      </>
-    )} 
-  </div>
-  )
+    <div className="app">
+      <div className="quiz-title">{quizzes[currentQuiz].title}</div>
+      {showSummary ? (
+        <Summary
+          score={score}
+          attempts={attempts}
+          quizTotal={quizzes[currentQuiz].questions}
+          handleNextQuizClick={handleNextQuizClick}
+          handleRetakeClick={handleRetakeClick}
+          getMessage={getMessage}
+        />
+      ) : (
+        <>
+          <br></br>
+          <div className="question-section">
+            <div className="question-text">
+              {quizzes[currentQuiz].questions[currentQuestion].text}
+            </div>
+            <br></br>
+            <div className="answer-section">
+              {answers.map((answer) => (
+                <li
+                  className={`answer-list${getClasses(answer)}`}
+                  key={`${currentQuestion}-${answer}`}
+                  onClick={(e) => handleAnswerClick(e, answer)}
+                >
+                  {answer}
+                </li>
+              ))}
+            </div>
+          </div>
+          <div className="feedback-and-button">
+            <h3
+              className="correct-incorrect-message"
+              style={{ display: answerFeedback ? 'block' : 'none' }}
+            >
+              {answerFeedback}
+            </h3>
+            <button
+              className="next-question"
+              style={{ display: nextButton ? 'block' : 'none' }}
+              onClick={() => handleNextQuestionClick()}
+            >
+              {' '}
+              Next{' '}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
-
-export default App; 
+export default App;
